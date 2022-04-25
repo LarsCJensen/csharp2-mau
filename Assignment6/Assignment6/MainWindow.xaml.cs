3e8ed783b410bc6a3164d75558315588cc476d32
larscjensen <lars.c.jensen@gmail.com>
2022-04-25 20:20:43 +0200

    Assignment 6

---
+++ Assignment6/Assignment6/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {        
+        private object dummyNode = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeGUI();
+        }
+        // TODO CLasses
+        // File, Icon, Name, Path, DateCreated, DateModified
+        // FileType (Enum), File, Folder, Drive
+        private void InitializeGUI()
+        {
+            // Välj katalog via fileDialog
+            // Kryssruta "Inkludera sub-folders"
+            // Vilka attribute att jämföra med?
+
+            AddLogicalDrives();
+        }
+        /// <summary>
+        /// Event for when folder is expanded
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FolderExpanded(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = (TreeViewItem)sender;
+            if (item.Items.Count == 1 && item.Items[0] == dummyNode)
+            {
+                item.Items.Clear();
+                try
+                {
+                    foreach (string s in Directory.GetDirectories(item.Tag.ToString()))
+                    {
+                        TreeViewItem subitem = new TreeViewItem();
+                        subitem.Header = s.Substring(s.LastIndexOf("\\") + 1);
+                        subitem.Tag = s;
+                        subitem.FontWeight = FontWeights.Normal;
+                        subitem.Items.Add(dummyNode);
+                        subitem.Expanded += new RoutedEventHandler(FolderExpanded);
+                        item.Items.Add(subitem);
+                    }
+                }
+                catch (Exception) { }
+            }
+        }
+        /// <summary>
+        /// Helper method to add logical drives to treeview
+        /// </summary>
+        private void AddLogicalDrives()
+        {
+            foreach (string s in Directory.GetLogicalDrives())
+            {
+                TreeViewItem item = new TreeViewItem();
+                item.Header = s;
+                item.Tag = s;
+                item.FontWeight = FontWeights.Normal;
+                // Adding a dummyNode to make first expand work
+                item.Items.Add(dummyNode);
+                item.Expanded += new RoutedEventHandler(FolderExpanded);
+                fileTree.Items.Add(item);
+            }
+        }
+        public void foldersItem_SelectedItemChanged(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
+
+        private void btnChooseFolder_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "BMP|*.bmp|JPG|*.jpg|PNG|*.png|All files (*.*)|*.*";
+            bool? result = dialog.ShowDialog();
+            if (result == true)
+            {
+                //imgAnimal.Source = new BitmapImage(new Uri(dialog.FileName, UriKind.Absolute));
+            }
+        }
+    }
+}
