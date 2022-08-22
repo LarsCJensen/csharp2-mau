072422efd7db833b0cc195b6057fae8790d62d09
larscjensen <lars.c.jensen@gmail.com>
2022-08-22 19:29:39 +0200

    Fix: Small cleanup

--- MyGames/MyGames/Views/MainWindow.xaml.cs
+++ MyGames/MyGames/Views/MainWindow.xaml.cs
@@ -1,22 +1,6 @@
-﻿using MyGames.Models;
-using System.Data.Entity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using MyGames.Helpers;
 using MyGames.ViewModels;
-using GalaSoft.MvvmLight.Messaging;
-using MyGames.Helpers;
+using System.Windows;
 
 namespace MyGames.Views
 {
